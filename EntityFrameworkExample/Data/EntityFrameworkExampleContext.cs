5b7b769a27598b4df5e7c8afdfed548542d8546f
Angel Valladares <arivalladares2.0@gmail.com>
2022-11-07 18:55:31 -0600

    first commit

---
+++ EntityFrameworkExample/Data/EntityFrameworkExampleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EntityFrameworkExample.Models;
+
+namespace EntityFrameworkExample.Data
+{
+    public class EntityFrameworkExampleContext : DbContext
+    {
+        public EntityFrameworkExampleContext (DbContextOptions<EntityFrameworkExampleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EntityFrameworkExample.Models.Customer> Customer { get; set; } = default!;
+    }
+}
